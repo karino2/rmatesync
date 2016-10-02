81ccb19a2cdf18f3790603295760a56233ca564a
karino2 <hogeika2@gmailcom>
2016-10-02 22:20:39 +0900

    open command is parsed.

---
+++ RMateSync/ConnectionHandler.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMateSync
+{
+
+    class OpenCommand
+    {
+        enum State
+        {
+            PARSE_PARAM,
+            PARSE_DATA,
+            FINISH
+        }
+
+        public String Path { set; get; }
+
+        UnbufferedStreamReader _reader;
+        NetworkStream _stream;
+        MemoryStream _data = new MemoryStream();
+        State _state = State.PARSE_PARAM;
+        Dictionary<String, String> _options = new Dictionary<string, string>();
+
+        public OpenCommand(UnbufferedStreamReader sr, NetworkStream ns)
+        {
+            _reader = sr;
+            _stream = ns;
+            DataLength = 0;
+        }
+
+        public bool IsFinish
+        {
+            get
+            {
+                return _state == State.FINISH;
+            }
+        }
+
+        public String DebToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach(var pair in _options)
+            {
+                builder.Append("option[");
+                builder.Append(pair.Key);
+                builder.Append("] = ");
+                builder.Append(pair.Value);
+                builder.Append("\n");
+            }
+            builder.Append("datalen:");
+            builder.Append(DataLength);
+            builder.Append("\n");
+            builder.Append(DebGetContents());
+            return builder.ToString();
+        }
+
+        public String DebGetContents()
+        {
+            long orgpos = _data.Position;
+            _data.Position= 0;
+            var tmpReader = new StreamReader(_data);
+            var res = tmpReader.ReadToEnd();
+            _data.Position = orgpos;
+            return res;
+        }
+
+        public int DataLength { set; get; }
+
+        public void HandleParam(string line)
+        {
+            var seps = line.Split(':');
+            var key = seps[0].Trim();
+            var val = seps[1].Trim();
+
+            if ("data" == key)
+            {
+                DataLength = int.Parse(val);
+                _state = State.PARSE_DATA;
+            } else
+            {
+                _options[key] = val;
+            }
+        }
+
+        public void ReadData()
+        {
+            int sizeRead = 0;
+            byte[] buf = new byte[1024];
+            
+            while(sizeRead < DataLength)
+            {
+                var count = _stream.Read(buf, 0, Math.Min(buf.Length, DataLength - sizeRead));
+                _data.Write(buf, 0, count);
+                sizeRead += count;
+            }
+
+            _state = State.FINISH;
+        }
+
+        public void ReadAndEvalOne()
+        {
+            switch(_state)
+            {
+                case State.PARSE_PARAM:
+                    HandleParam(_reader.ReadLine());                    
+                    return;
+                case State.PARSE_DATA:
+                    ReadData();
+                    return;
+                case State.FINISH:
+                    return;
+            }
+        }
+
+
+    }
+
+    class UnbufferedStreamReader
+    {
+        Stream _stream;
+        public UnbufferedStreamReader(Stream basestream)
+        {
+            _stream = basestream;
+        }
+
+        public string ReadLine()
+        {
+            List<byte> bytes = new List<byte>();
+            int current;
+            while ((current = _stream.ReadByte()) != -1 && current != (int)'\n')
+            {
+                byte b = (byte)current;
+                bytes.Add(b);
+            }
+            return Encoding.ASCII.GetString(bytes.ToArray());
+        }
+
+
+    }
+
+    class ConnectionHandler
+    {
+        TcpClient _client;
+        UnbufferedStreamReader _reader;
+        StreamWriter _writer;
+        NetworkStream _stream;
+        public ConnectionHandler(TcpClient client, NetworkStream ns, StreamReader sr, StreamWriter sw)
+        {
+            _client = client;
+            _reader = new UnbufferedStreamReader(ns);
+            _stream = ns;
+            _writer = sw;
+        }
+
+        public void DoProcesss()
+        {
+            String msg = _reader.ReadLine();
+            Debug.WriteLine(msg); // "open"
+
+            var cmd = new OpenCommand(_reader, _stream);
+            while(!cmd.IsFinish)
+            {
+                cmd.ReadAndEvalOne();
+                Debug.WriteLine(cmd.DebToString());
+            }
+            Debug.WriteLine("done");
+
+        }
+
+    }
+}
--- RMateSync/MainWindow.xaml.cs
+++ RMateSync/MainWindow.xaml.cs
@@ -37,97 +37,21 @@ namespace RMateSync
             dp("do nothing now");
         }
 
-        public ManualResetEvent tcpClientConnected =
-            new ManualResetEvent(false);
-
 
         void test1()
         {
             dp("test1 start");
-            // Parse("127.0.0.1")
             var listener = new TcpListener(IPAddress.Loopback, PORT_NUM);
-            // var listener = new TcpListener(IPAddress.Any, PORT_NUM);
 
             listener.Start();
             while (true)
             {
-                /*
-                listener.BeginAcceptTcpClient(
-                    new AsyncCallback(TcpClientCallback),
-                    listener);
-                    */
                 var client = listener.AcceptTcpClient();
-                // var client = listener.AcceptSocket();
                 DebugDump(client);
 
                 Thread.Sleep(100);
-                // tcpClientConnected.WaitOne();
             }
 
-        }
-
-        private void TcpClientCallback(IAsyncResult ar)
-        {
-            var listener = (TcpListener)ar.AsyncState;
-
-            TcpClient client = listener.EndAcceptTcpClient(ar);
-
-            client.ReceiveBufferSize = 1;
-
-            DebugDump(client);
-
-            tcpClientConnected.Set();
-        }
-
-        public class StateObject
-        {
-            // Client socket.
-            public Socket workSocket = null;
-            // Size of receive buffer.
-            public const int BufferSize = 256;
-            // Receive buffer.
-            public byte[] buffer = new byte[BufferSize];
-            // Received data string.
-            public StringBuilder sb = new StringBuilder();
-        }
-
-        void DebugDumpS(Socket client)
-        {
-            dp("dds1");
-            StateObject state = new StateObject();
-            state.workSocket = client;
-
-            // Begin receiving the data from the remote device.
-            client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                new AsyncCallback(ReceiveCallback), state);
-        }
-
-        private void ReceiveCallback(IAsyncResult ar)
-        {
-            dp("dds2");
-            // Retrieve the state object and the client socket 
-            // from the asynchronous state object.
-            StateObject state = (StateObject)ar.AsyncState;
-            Socket client = state.workSocket;
-            // Read data from the remote device.
-            int bytesRead = client.EndReceive(ar);
-            dp("dds3");
-            if (bytesRead > 0)
-            {
-                String responseData = System.Text.Encoding.ASCII.GetString(state.buffer, 0, bytesRead);
-                debugWriteLine(responseData);
-
-
-                /*
-                //  Get the rest of the data.
-                client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                    new AsyncCallback(ReceiveCallback), state);
-                    */
-            }
-            else
-            {
-                dp("bytesread not positive");
-            }
         }
 
         void DebugDump(TcpClient client)
@@ -139,55 +63,18 @@ namespace RMateSync
             {
                 sw.WriteLine("MyServer Name");
                 sw.Flush();
+                ConnectionHandler handler = new ConnectionHandler(client, stream, sr, sw);
+                handler.DoProcesss();
+
+                /*
                 string line = sr.ReadLine();
                 dp(line);
+                */
             }
+            client.Close();
             // dp("dd2");
         }
 
-        void DebugDump2(TcpClient client)
-        {
-            /*
-            using (NetworkStream stream = client.GetStream())
-            {
-                dp("deb2");
-                */
-
-
-                Byte[] data = new Byte[10];
-                // while (stream.CanRead) {
-                    /*
-                        if(!stream.DataAvailable)
-                        {
-                            Thread.Sleep(100);
-                        }else { 
-                        {
-                        */
-                    /*
-                    client.Available
-                    dp(client.Available.ToString());
-                    return;
-                    */
-                    // Int32 bytes = stream.Read(data, 0, Math.Min(data.Length, client.Available));
-                    /*
-                    stream.ReadTimeout = 500;
-                    try { 
-                        Int32 bytes = stream.Read(data, 0, 1);
-                        String responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                        debugWriteLine(responseData);
-                    }catch(IOException e)
-                        {
-                            dp("timeout");
-                        }
-                        */
-                        // return;
-
-            /*        
-                }
-                dp("deb3");
-            }
-            */
-        }
 
         private void dp(String msg)
         {
